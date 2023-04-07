c2360f52f80d37cb32638a32ebb4d26306b50dd2
anish.jeyanth1 <anish.jeyanth1@ie.ey.com>
2023-04-07 21:00:46 +0100

    circular linked list

---
+++ 3-LinkedList/CircularSingleLinkedList.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.LinkedList
+{
+    public class CircularSingleLinkedList
+    {
+        public Node Head;
+        public Node Tail;
+        int Size;
+
+        public Node CreateLinkedList(int data)
+        {
+            Node node = new Node(data);
+            Head = node;
+            Tail = node;
+            Size = 1;
+
+            return Head;
+        }
+
+        public void InsertNode(int data, int location = 0)
+        {
+            Node node = new Node(data);
+            if (Head == null)
+            {
+                CreateLinkedList(100);
+            }
+            else if(location == 0)
+            {
+                node.Next = Head;
+                Head = node;
+                Tail.Next = Head;
+            }
+            else if(location >= Size)
+            {
+                Tail.Next = node;
+                Tail = node;
+                Tail.Next = Head;
+            }   
+            else
+            {
+                Node tempNode = Head;
+                int index = 0;
+                while (index < location -1)
+                {
+                    tempNode = tempNode.Next;
+                    index++;
+                }
+
+                node.Next = tempNode.Next;
+                tempNode.Next = node;
+            }
+
+            Size++;
+        }
+
+        public void DeleteNode(int location)
+        {
+            if(Size == 0)
+            {
+                Head.Next = null;
+                Head = null;
+                Tail = null;                
+            }
+            else if(location == 0)
+            {
+                Head = Head.Next;
+                Tail.Next = Head;
+            }
+            else if (location >= Size)
+            {
+                Node tempNode = Head;
+                for(int i=0;i < Size -1; i++)
+                {
+                    tempNode = tempNode.Next;
+                }
+
+                tempNode.Next = Head;
+                Tail = tempNode;
+            }
+            else
+            {
+                Node tempNode = Head;
+                for (int i = 0; i < location - 1; i++)
+                {
+                    tempNode = tempNode.Next;
+                }
+
+                tempNode.Next = tempNode.Next.Next;
+            }
+
+            Size--;
+        }
+
+
+        public void Traverse()
+        {
+            Node tempNode = Head;
+            do
+            {
+                Console.Write(tempNode.Value);
+                tempNode = tempNode.Next;
+                Console.Write("->");
+            } while (tempNode != Head);
+
+            Console.WriteLine("");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,21 +1,37 @@
-﻿DSA.LinkedList.SingleLinkedList singleLinkedList = new DSA.LinkedList.SingleLinkedList();
-singleLinkedList.CreateLinkedList(100);
-singleLinkedList.InserNode(200);
-singleLinkedList.InserNode(300);
-singleLinkedList.InserNode(400);
-singleLinkedList.InserNode(800, 8);
-singleLinkedList.InserNode(500);
-singleLinkedList.InserNode(600);
-singleLinkedList.InserNode(450,2);
-singleLinkedList.InserNode(900, 9);
-singleLinkedList.Travesrse();
-Console.WriteLine(singleLinkedList.FindNode(200));
-singleLinkedList.DeleteNode(2);
-singleLinkedList.Travesrse();
-singleLinkedList.DeleteNode(1);
-singleLinkedList.Travesrse();
-singleLinkedList.DeleteNode(10);
-singleLinkedList.Travesrse();
+﻿DSA.LinkedList.CircularSingleLinkedList circularSingleLinkedList = new DSA.LinkedList.CircularSingleLinkedList();
+circularSingleLinkedList.CreateLinkedList(100);
+circularSingleLinkedList.InsertNode(200);
+circularSingleLinkedList.InsertNode(300);
+circularSingleLinkedList.InsertNode(250, 1);
+circularSingleLinkedList.InsertNode(500,9);
+circularSingleLinkedList.InsertNode(400);
+circularSingleLinkedList.Traverse();
+circularSingleLinkedList.DeleteNode(0);
+circularSingleLinkedList.Traverse();
+circularSingleLinkedList.DeleteNode(10);
+circularSingleLinkedList.Traverse();
+circularSingleLinkedList.DeleteNode(1);
+circularSingleLinkedList.Traverse();
+
+
+//DSA.LinkedList.SingleLinkedList singleLinkedList = new DSA.LinkedList.SingleLinkedList();
+//singleLinkedList.CreateLinkedList(100);
+//singleLinkedList.InserNode(200);
+//singleLinkedList.InserNode(300);
+//singleLinkedList.InserNode(400);
+//singleLinkedList.InserNode(800, 8);
+//singleLinkedList.InserNode(500);
+//singleLinkedList.InserNode(600);
+//singleLinkedList.InserNode(450,2);
+//singleLinkedList.InserNode(900, 9);
+//singleLinkedList.Travesrse();
+//Console.WriteLine(singleLinkedList.FindNode(200));
+//singleLinkedList.DeleteNode(2);
+//singleLinkedList.Travesrse();
+//singleLinkedList.DeleteNode(1);
+//singleLinkedList.Travesrse();
+//singleLinkedList.DeleteNode(10);
+//singleLinkedList.Travesrse();
 
 
 //DSA.Array.Array.RotateMatrix(new int[4,4] { { 1, 2, 3, 4 }, { 5, 6, 7,8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16} });
